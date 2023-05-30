2cf00f40c0c3b85cf86ca920d5838c633e81c70a
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-05-30 20:49:53 +0530

    twoSum

--- DSA/Lecture 01- Arrays/twoSum.js
+++ DSA/Lecture 01- Arrays/twoSum.js
@@ -1,22 +1,3 @@
-# Assignment 01- Arrays
-
-## 💡 Question 01
-
-> Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-> You may assume that each input would have exactly one solution, and you may not use the same element twice.
->
-> You can return the answer in any order.
->
-> **Example:**
->
-> - Input: nums = [2, 7, 11, 15], target = 9
-> - Output: [0, 1]
->
-> Explanation: Because nums[0] + nums[1] == 9, we return [0, 1]
-
-### 🚀 Answer
-
-```javascript
 const twoSum = (nums, target) => {
   let ans = []; // to store the indices of the two numbers
   for (let i = 0; i < nums.length; i++) {
@@ -34,4 +15,5 @@ const twoSum = (nums, target) => {
   }
   return ans; // if no such numbers exist, return an empty array
 };
-```
+
+console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
